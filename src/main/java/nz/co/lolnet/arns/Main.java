dc7c0db7e2fb737338fa41221f7c426e8449dcc5
Arns <mcprogaming5@gmail.com>
2015-02-11 11:48:11 +1300

    ..

--- src/main/java/nz/co/lolnet/arns/Main.java
+++ src/main/java/nz/co/lolnet/arns/Main.java
@@ -1,9 +1,39 @@
 package nz.co.lolnet.arns;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  *
  * @author Arns
  */
-public class Main {
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() { 
+        getLogger().info("Hello Console,How are you today? Parkour has been enabled!");   
+                           }  
     
-}
+    public void onDisable() {
+        getLogger().info("Bye Console! Parkour has been disabled :'( ");
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {    
+        
+        if (cmd.getName().equalsIgnoreCase("parkour help") && sender instanceof Player) {
+        
+            Player player = (Player) sender;
+            
+            player.sendMessage("Commands coming soon!");
+            
+        }        
+        
+        return false;
+       }
+
+    
+    
+    
+    
+}       
