1a7dc50888c7246fe70b7470854ae1da397bfffb
James Anderson <james137137@gmail.com>
2015-02-11 11:54:11 +1300

    adjusted for the command to work. (command can't contain spaces)

--- src/main/java/nz/co/lolnet/arns/Main.java
+++ src/main/java/nz/co/lolnet/arns/Main.java
@@ -10,30 +10,31 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Arns
  */
 public class Main extends JavaPlugin {
+
     @Override
-    public void onEnable() { 
-        getLogger().info("Hello Console,How are you today? Parkour has been enabled!");   
-                           }  
-    
+    public void onEnable() {
+        getLogger().info("Hello Console,How are you today? Parkour has been enabled!");
+    }
+
     public void onDisable() {
         getLogger().info("Bye Console! Parkour has been disabled :'( ");
     }
-    
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {    
-        
-        if (cmd.getName().equalsIgnoreCase("parkour help") && sender instanceof Player) {
-        
-            Player player = (Player) sender;
-            
-            player.sendMessage("Commands coming soon!");
-            
-        }        
-        
-        return false;
-       }
 
-    
-    
-    
-    
-}       
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (cmd.getName().equalsIgnoreCase("parkour") && sender instanceof Player) {
+
+            Player player = (Player) sender;
+            if (args.length >= 1) {
+                if (args[0].equalsIgnoreCase("help")) {
+                    player.sendMessage("Commands coming soon!");
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+}
