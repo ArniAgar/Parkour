7dd5d6c31528180321ba84332831782650473cdf
James Anderson <james137137@gmail.com>
2015-02-11 14:50:43 +1300

    added start of wanding

--- src/main/java/nz/co/lolnet/arns/parkour/MyListener.java
+++ src/main/java/nz/co/lolnet/arns/parkour/MyListener.java
@@ -5,7 +5,10 @@
  */
 package nz.co.lolnet.arns.parkour;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
  *
@@ -15,5 +18,16 @@ class MyListener implements Listener {
 
     public MyListener(Main aThis) {
     }
-    
+
+    @EventHandler
+    public void onRightClickBlockWithWand(PlayerInteractEvent event) {
+        if (event.getPlayer().hasPermission("somerandomPermission.node.here")) {
+            if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && 
+                    event.hasBlock() && 
+                    event.getPlayer().getItemInHand().getType().equals(Main.wand.getType())) {
+                //Do something
+            }
+        }
+
+    }
 }
