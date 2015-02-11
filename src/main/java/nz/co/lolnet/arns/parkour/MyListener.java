de1e456f4f90a05496b8e6e6d434553f571fb149
James Anderson <james137137@gmail.com>
2015-02-11 14:25:21 +1300

    Added Listener class

---
+++ src/main/java/nz/co/lolnet/arns/parkour/MyListener.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.lolnet.arns.parkour;
+
+import org.bukkit.event.Listener;
+
+/**
+ *
+ * @author James
+ */
+class MyListener implements Listener {
+
+    public MyListener(Main aThis) {
+    }
+    
+}
