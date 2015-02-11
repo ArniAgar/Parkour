beedd5ac041f33b2af19f938b9f4a31387ac089a
James Anderson <james137137@gmail.com>
2015-02-11 15:06:23 +1300

    messing a }

--- src/main/java/nz/co/lolnet/arns/parkour/Main.java
+++ src/main/java/nz/co/lolnet/arns/parkour/Main.java
@@ -12,26 +12,26 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Arns with helpof James137137
  */
 public class Main extends JavaPlugin {
-    
+
     public static ItemStack wand;
-    
+
     @Override
     public void onEnable() {
         getLogger().info("Hello Console,How are you today? Parkour has been enabled!");
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new MyListener(this), this);
         wand = new ItemStack(Material.getMaterial(this.getConfig().getInt("WandItem")));
-        
+
     }
-    
+
     public void onDisable() {
         getLogger().info("Bye Console! Parkour has been disabled :'( ");
     }
-    
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        
+
         if (cmd.getName().equalsIgnoreCase("parkour") && sender instanceof Player) {
-            
+
             Player player = (Player) sender;
             if (args.length >= 1) {
                 if (args[0].equalsIgnoreCase("help")) {
@@ -42,11 +42,12 @@ public class Main extends JavaPlugin {
                     player.sendMessage("You can run the follwing commands:");
                     player.sendMessage("/parkour select - This will allow you to select the area the parkour is in!");
                     player.sendMessage("/parkour addWaypoint - This allows you to select a waypoint so when a player dies they will go back to that point!");
+                }
+
             }
-            
+
         }
-        
         return false;
     }
-    
+
 }
